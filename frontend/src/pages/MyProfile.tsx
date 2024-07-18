b8afded00601a34aa884ae1354022de2f7255910
Aryan Dumyan <140326299+HirsuiteCrown@users.noreply.github.com>
2024-07-18 17:34:18 +0530

    Update MyProfile.tsx

--- frontend/src/pages/MyProfile.tsx
+++ frontend/src/pages/MyProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Appbar } from '../components/Appbar';
 import { BlogCard, Avatar } from '../components/BlogCard';
